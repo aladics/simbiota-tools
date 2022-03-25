23b63513768d762d1f1509615d262defdb2a4bd5
Tamas Aladics <aladics@inf.u-szeged.hu>
2022-03-25 08:07:37 +0100

    Added reset function, extended readme.

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 ## Setup:
 - Start DWF anaconda environment
 - Copy and rename `config_default.yaml` to `config.yaml` and set the values accordingly (hyper res and hyper cache dirs must be created manually)
+- If previous runs were ran:
+
+```Bash 
+python -m common.reset_all
+```
 
 ## Usgage
 Run commands from root.
@@ -13,15 +18,21 @@ Run commands from root.
 python -m cdf.run
 ```
 
+Results will be stored as specified in the config file's `cdf_results_dir` param.
+
 ### Run Hyperparam search
+Run cdf generator first. Then:
 
 ```Bash
 python -m hyper.run
 ```
 
+Results will be stored as specified in the config file's `hyper_results_dir` param.
+
 ### Run figure generator
-Generate figures containing the histogram and the plotted CDFs&PDFs.
+Generate figures containing the histogram and the plotted CDFs and PDFs.
 
 ```Bash
 python -m cdf.create_figures.run
 ```
+By default results will be in `cdf/create_figures/results`
---
+++ common/reset_all.py
@@ -0,0 +1,33 @@
+from email.policy import default
+from common import util
+from pathlib import Path
+import shutil
+
+import click
+
+def recreate_empty_dir(dir_path: Path):
+    """ Deletes dir_path if exists then recreates it as empty. """
+    
+    if dir_path.exists():
+        shutil.rmtree(str(dir_path.resolve()))
+    dir_path.mkdir(exist_ok=True, parents=True)
+
+@click.command()
+@click.option("--mode", type=click.Choice(['all', 'hyper', 'data', 'cdf']), case_sensitive = False, default = 'all')
+def run(mode):
+    config = util.get_config()
+    
+    if mode == "all" or mode == "data":
+        Path(config['result_train_csv']).unlink()
+        Path(config['result_test_csv']).unlink()
+    
+    if mode == "all" or mode == "cdf":
+        recreate_empty_dir(Path(config['cdf_results_dir']))
+        recreate_empty_dir(Path(config['cdf_func_cache_dir']))
+        
+    if mode == "all" or mode == "hyper":
+        recreate_empty_dir(Path(config['hyper_results_dir']))
+    
+    
+if __name__ == "__main__":
+    run()
